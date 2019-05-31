1ee2470c4b746bd0f1c3d207cce5ea431496c067
codemobiles <chaiyasit.t@gmail.com>
2019-05-31 16:41:31 +0700

    first commit

---
+++ DKImagePickerControllerDemo/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  DKImagePickerControllerDemo
+//
+//  Created by ZhangAo on 14-10-1.
+//  Copyright (c) 2014年 ZhangAo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    } 
+
+}
+
